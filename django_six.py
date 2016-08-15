4f0cd60e24ebd28d2d5fa8d5cbc0c7a59397df01
Brightcells <brightcells@gmail.com>
2016-08-15 13:29:44 +0800

    add CompatibilityBaseCommand etc

--- django_six.py
+++ django_six.py
@@ -1,5 +1,11 @@
 # -*- coding: utf-8 -*-
 
+from optparse import make_option
+
+import django
+from django.core.management.base import AppCommand, BaseCommand, CommandError, LabelCommand
+
+
 # https://github.com/django/django/blob/fc94944183f1f1325824ee0ef1f49d737ec86d4a/django/db/__init__.py#L101-L112
 # close_connection is superseded by close_old_connections
 # RemovedInDjango18Warning
@@ -9,3 +15,38 @@ except ImportError:
     from django.db import close_connection
 
 close_old_connections = close_connection
+
+
+# BaseCommand Series
+class ProxyParser(object):
+    """Faux parser object that will ferry our arguments into options."""
+
+    def __init__(self, command):
+        self.command = command
+
+    def add_argument(self, *args, **kwargs):
+        self.command.option_list += (make_option(*args, **kwargs), )
+
+
+class CompatibilityBaseCommand(BaseCommand):
+    """Provides a compatibility between optparse and argparse transition.
+    Starting in Django 1.8, argparse is used. In Django 1.9, optparse support will be removed.
+    For optparse, you append to the option_list class attribute.
+    For argparse, you must define add_arguments(self, parser).
+    BaseCommand uses the presence of option_list to decide what course to take.
+    """
+
+    def __init__(self, *args, **kwargs):
+        if django.VERSION < (1, 8) and hasattr(self, 'add_arguments'):
+            self.option_list = BaseCommand.option_list
+            parser = ProxyParser(self)
+            self.add_arguments(parser)
+        super(CompatibilityBaseCommand, self).__init__(*args, **kwargs)
+
+
+class CompatibilityAppCommand(AppCommand, CompatibilityBaseCommand):
+    """AppCommand is a BaseCommand sub-class without its own __init__."""
+
+
+class CompatibilityLabelCommand(LabelCommand, CompatibilityBaseCommand):
+    """LabelCommand is a BaseCommand sub-class without its own __init__."""
